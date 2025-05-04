12fff4a70a3dfd982c0db5b6283ef6f18deb3232
GusLuis <159668789+GusLuis@users.noreply.github.com>
2025-05-04 18:23:23 -0300

    update src

---
+++ aula04/script.js
@@ -0,0 +1,9 @@
+ // Janela de Alerta
+ window.alert('Minha Primeira Mensagem')
+
+ // Janela de Confirmação
+ window.confirm('Está gostando do script?')
+
+ // Janela de prompt
+ // Pode ser usado para fazer pergunta
+ window.prompt('Quão é o seu nome?')
